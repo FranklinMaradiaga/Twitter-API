69e56092f8fe2bfd0468efe69772d39f0c3977ab
Franklin S Maradiaga <franklin.maradiaga@lc.cuny.edu>
2022-07-03 21:44:05 +0000

    updated commit

--- twitter.py
+++ twitter.py
@@ -1,73 +1,53 @@
 from pytwitter import Api
 from cryptography.x509 import load_pem_x509_certificate
-# import requests
 import pandas as pd
+import sqlalchemy as db
 
-CONSUMER_KEY = 'UL1po4lsawfWRubl2ecIs16FP'
-CONSUMER_SECRET = 'TkzqSUKd81xDuZr3pTR6kxgBZFhJiPM2XVz6OatBOkqaJL2dB5'
 BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAAGBLeQEAAAAAaHebLkgV5Z9RTfB9uh1u%2FZp3iKo%3DVVj15lNsi2yfaslS0l6CffwelmWPUCvNN4efJOfiRTxc7h9AeZ'
-ACCESS_TOKEN = '1542548435819266055-giRnvWw97Pz0BNTTJ9wCV82yF71pwz'
-ACCESS_TOKEN_SECRET = 'CAaqwq9fqYRDK1rjRghqtxlqnirtv7oIPwqZVcTnzKEoL'
 
 my_api = Api(bearer_token = BEARER_TOKEN)
 
-response = my_api.get_following(user_id="44196397", max_results=5)
+# This should return a dictionary of the follow
+# The parameters should all be of type list([])
+def get_dict(ids, names, usernames):
 
-ids = []
-names = []
-usernames = []
-
-for data in response.data:
-    ids.append(data.id)
-    names.append(data.name)
-    usernames.append(data.username)
-
-followers = {
+    dict = {
               'ids' : ids,
               'names' : names,
               'usernames' : usernames
-            }
+           }
+    return dict
 
-print("\nids:", followers['ids'])
-print("\nnames:", followers['ids'])
-print("\nusernames:", followers['ids'])
+# This should return a dictionary by calling the get_dict
+# The id must be a of type string and max_users should be of type int 
+def get_following_users(id, max_users):
+  response = my_api.get_following(user_id=id, max_results=max_users)
+  ids = []
+  names = []
+  usernames = []
+
+  for data in response.data:
+      ids.append(data.id)
+      names.append(data.name)
+      usernames.append(data.username)
+
+  return get_dict(ids, names, usernames)
+
+# The info must be a dictionary to add to the database, d
+# b_name, and table_name should be of type string
+def create_database(info, db_name, table_name):
+
+  data = pd.DataFrame.from_dict(followers)
+  engine = db.create_engine('sqlite:///' + db_name + '.db')
+
+  data.to_sql(table_name, con=engine, if_exists='replace', index=False)
+  query_result = engine.execute("SELECT * FROM " + table_name + ";").fetchall()
+  # print(pd.DataFrame(query_result), "\n")
 
 
-
-
-
-
-
-
-
-
+followers = get_following_users("44196397", 5)
+# print(followers)
+create_database(followers, "Twitter", "followers")
 # print(my_api.get_user(user_id="44196397"))
-# print(author_id("44196397"))
-# print(type(my_api.get_following(user_id="3762209068")))
-# following  pd.DataFrame.from_dict(response)
-# l = list(response)
 
-
-
-
-
-
-
-
-
-
-
-# import tweepy
-
-# CONSUMER_KEY = 'UL1po4lsawfWRubl2ecIs16FP'
-# CONSUMER_SECRET = 'TkzqSUKd81xDuZr3pTR6kxgBZFhJiPM2XVz6OatBOkqaJL2dB5'
-# BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAAGBLeQEAAAAAaHebLkgV5Z9RTfB9uh1u%2FZp3iKo%3DVVj15lNsi2yfaslS0l6CffwelmWPUCvNN4efJOfiRTxc7h9AeZ'
-# ACCESS_TOKEN = '1542548435819266055-giRnvWw97Pz0BNTTJ9wCV82yF71pwz'
-# ACCESS_TOKEN_SECRET = 'CAaqwq9fqYRDK1rjRghqtxlqnirtv7oIPwqZVcTnzKEoL'
-
-# # create your client 
-# client = tweepy.Client(bearer_token=BEARER_TOKEN)
-
-# search_query = "#covid19 -in:retweets"
-
-# query = "#covid19 lang:en -is:retweet"
+# print(my_api.get_following(user_id="44196397", max_results=5))
