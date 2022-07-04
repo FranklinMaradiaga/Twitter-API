bc68f026a82364d10c68220f1d77757174a7d4d0
Franklin S Maradiaga <franklin.maradiaga@lc.cuny.edu>
2022-07-04 06:33:12 +0000

    updated commit

--- twitter.py
+++ twitter.py
@@ -3,15 +3,14 @@ from cryptography.x509 import load_pem_x509_certificate
 import pandas as pd
 import sqlalchemy as db
 
-first = "AAAAAAAAAAAAAAAAAAAAAGBLeQEAAAAAaHebLkgV5Z9RTfB9uh1u%2FZp3iKo%3DVVj15lNsi2yfas"
-last = "lS0l6CffwelmWPUCvNN4efJOfiRTxc7h9AeZ"
+first = "AAAAAAAAAAAAAAAAAAAAAGBLeQEAAAAAaHebLkgV5Z9RTfB9uh1u%2FZp3i"
+last = "Ko%3DVVj15lNsi2yfaslS0l6CffwelmWPUCvNN4efJOfiRTxc7h9AeZ"
 
 BEARER_TOKEN = first + last
 my_api = Api(bearer_token=BEARER_TOKEN)
 
 # This should return a dictionary of the follow
 # The parameters should all be of type list([])
-
 def get_dict(ids, names, usernames):
 
     dict = {
@@ -25,7 +24,7 @@ def get_dict(ids, names, usernames):
 # This should return a dictionary by calling the get_dict
 # The id must be a of type string and max_users should be of type int
 def get_following_users(id, max_users):
-  
+
     response = my_api.get_following(user_id=id, max_results=max_users)
     ids = []
     names = []
