87a06c3834dcfee60dfef8ff193c5290e9dee178
Franklin S Maradiaga <franklin.maradiaga@lc.cuny.edu>
2022-07-04 06:21:34 +0000

    updated commit

--- twitter.py
+++ twitter.py
@@ -6,21 +6,21 @@ import sqlalchemy as db
 BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAGBLeQEAAAAAaHebLkgV5Z9RTfB9uh1u%2FZp3iKo%3DVVj15lNsi2yfasl" \
                "S0l6CffwelmWPUCvNN4efJOfiRTxc7h9AeZ"
 
-my_api = Api(bearer_token = BEARER_TOKEN)
+my_api = Api(bearer_token=BEARER_TOKEN)
 
-# This should return a dictionary of the follow
-# The parameters should all be of type list([])
+#This should return a dictionary of the follow
+#The parameters should all be of type list([])
 def get_dict(ids, names, usernames):
 
     dict = {
-        'ids' : ids,
-        'names' : names,
-        'usernames' : usernames
+        'ids': ids,
+        'names': names,
+        'usernames': usernames
     }
     return dict
 
-# This should return a dictionary by calling the get_dict
-# The id must be a of type string and max_users should be of type int 
+#This should return a dictionary by calling the get_dict
+#The id must be a of type string and max_users should be of type int 
 def get_following_users(id, max_users):
     response = my_api.get_following(user_id=id, max_results=max_users)
     ids = []
@@ -34,8 +34,8 @@ def get_following_users(id, max_users):
 
     return get_dict(ids, names, usernames)
 
-# The info must be a dictionary to add to the database, d
-# b_name, and table_name should be of type string
+#The info must be a dictionary to add to the database, d
+#b_name, and table_name should be of type string
 def create_database(info, db_name, table_name):
 
     data = pd.DataFrame.from_dict(followers)
@@ -47,5 +47,4 @@ def create_database(info, db_name, table_name):
 
 
 followers = get_following_users("44196397", 5)
-# print(followers)
 create_database(followers, "Twitter", "followers")
