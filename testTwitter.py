78732d12292d5c27fd5f66ff414683e19528d8de
Franklin S Maradiaga <franklin.maradiaga@lc.cuny.edu>
2022-07-05 05:42:22 +0000

    updated testTwitter file

--- testTwitter.py
+++ testTwitter.py
@@ -5,11 +5,13 @@ from twitter import get_following_users, get_dict, create_database
 class TestTwitter(unittest.TestCase):
 
     def test_get_following_users(self):
-      
+
         dict = {
-            'ids' : ['34719119', '1605', '18989355', '300878435', '541882699'],
-            'names' : ['Walter Isaacson', 'Sam Altman', 'Mike Solana', 'Dilbert', 'Andrea Stroppa'],
-            'username' : ['WalterIsaacson', 'sama', 'micsolana', 'Dilbert_Daily', 'Andst7']
+            'ids': ['34719119', '1605', '18989355', '300878435', '541882699'],
+            'names': ['Walter Isaacson', 'Sam Altman', 'Mike Solana', 'Dilbert', 
+            'Andrea Stroppa'],
+            'username': ['WalterIsaacson', 'sama', 'micsolana', 'Dilbert_Daily', 
+            'Andst7']
        }
 
         self.assertEqual(get_following_users("44196397", 5), dict)
