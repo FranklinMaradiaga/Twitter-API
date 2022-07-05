6dc50b8ee2febd25ae5540b696f506077e72a75b
Franklin S Maradiaga <franklin.maradiaga@lc.cuny.edu>
2022-07-05 05:44:51 +0000

    updated testTwitter file

--- testTwitter.py
+++ testTwitter.py
@@ -8,11 +8,11 @@ class TestTwitter(unittest.TestCase):
 
         dict = {
             'ids': ['34719119', '1605', '18989355', '300878435', '541882699'],
-            'names': ['Walter Isaacson', 'Sam Altman', 'Mike Solana', 'Dilbert', 
-            'Andrea Stroppa'],
+            'names': ['Walter Isaacson', 'Sam Altman', 'Mike Solana',
+            'Dilbert', 'Andrea Stroppa'],
             'username': ['WalterIsaacson', 'sama', 'micsolana', 'Dilbert_Daily', 
             'Andst7']
-       }
+        }
 
         self.assertEqual(get_following_users("44196397", 5), dict)
 
